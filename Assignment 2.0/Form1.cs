77f9ccd6200e09343416e4236faeeb9834c951fa
rc0032090 <rc0032090@CRO-24-09>
2019-06-04 10:53:51 +0100

    Copy and pasted into new project

--- Assignment 2.0/Form1.cs
+++ Assignment 2.0/Form1.cs
@@ -16,5 +16,27 @@ namespace Assignment_2._0
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double R1, R2, R3, R4, Resistance;
+            R1 = Convert.ToDouble(textBox1.Text);
+            R2 = Convert.ToDouble(textBox2.Text);
+            R3 = Convert.ToDouble(textBox3.Text);
+            R4 = Convert.ToDouble(textBox4.Text);
+            Resistance = R1 + R2 + R3 + R4;
+            label5.Text = "Resistance" + Resistance;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double R1, R2, R3, R4, Resistance;
+            R1 = Convert.ToDouble(textBox5.Text);
+            R2 = Convert.ToDouble(textBox6.Text);
+            R3 = Convert.ToDouble(textBox7.Text);
+            R4 = Convert.ToDouble(textBox8.Text);
+            Resistance = 1 / (1 / R1 + 1 / R2 + 1 / R3 + 1 / R4);
+            label10.Text = "Resistance" + Resistance;
+        }
     }
 }
